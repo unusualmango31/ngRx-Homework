182af8790d0e87f3f7d3fe39e0413d6b8988e53b
UnusualMango <unusualmango3131@gmail.com>
2021-04-24 16:32:05 +0300

    fixed birthday sort and add reset button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,66 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { element } from "protractor";
 import * as studentsJSON from "./students.json";
-import { element } from 'protractor';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   title: string = "Angular-homework";
   students: Array<StudentsArgs> = studentsJSON.students;
   filteredStudents: Array<StudentsArgs> = this.students;
   nameForSearch: string = "";
-  isShowBadRate: boolean = true;
   popupDisplay: string = "none";
+  isShowBadRate: boolean = true;
+  isResetRate: boolean = false;
+  isResetBirthday: boolean = false;
   private studentId: number;
   private isModalVisable: boolean = false;
   private isReverseSort: boolean = false;
 
   sortElement(sortedElementName: string): void {
     this.isReverseSort = this.toggleElement(this.isReverseSort);
-    if (!this.isReverseSort) {
-      this.filteredStudents.sort((a, b) => {
-        return a[sortedElementName] > b[sortedElementName] ? -1 : 1;
-      });
+
+    if (sortedElementName === "birthday") {
+      if (!this.isReverseSort) {
+        this.filteredStudents.sort((a, b) => {
+          return this.convertToDate( a[sortedElementName] ) > this.convertToDate( (b[sortedElementName]) ) ? -1 : 1;
+        });
+      } else {
+        this.filteredStudents.sort((a, b) => {
+          return this.convertToDate( a[sortedElementName] ) > this.convertToDate( (b[sortedElementName]) ) ? 1 : -1;
+        });
+      }
     } else {
-      this.filteredStudents.sort((a, b) => {
-        return a[sortedElementName] > b[sortedElementName] ? 1 : -1;
-      });
+      if (!this.isReverseSort) {
+        this.filteredStudents.sort((a, b) => {
+          return a[sortedElementName] > b[sortedElementName] ? -1 : 1;
+        });
+      } else {
+        this.filteredStudents.sort((a, b) => {
+          return a[sortedElementName] > b[sortedElementName] ? 1 : -1;
+        });
+      }
     }
   }
   filterStudents(filteredItemName: string, value: string): void {
     if (filteredItemName === "averageRate") {
+      this.isResetBirthday = true;
+      this.isResetRate = false;
       this.filteredStudents = this.students.filter( (e) => e[filteredItemName] > Number(value));
     } else if (filteredItemName === "birthday") {
+      this.isResetRate = true;
+      this.isResetBirthday = false;
       this.filteredStudents = this.students.filter( (e) => {
         return e[filteredItemName].substr(6, 4) === value;
       });
     }
   }
-  toggleElement( toggledElement: boolean): boolean {
-    if (toggledElement) {
-      return false;
-    }
-    return true;
+  resetStudents(): void {
+    this.filteredStudents = this.students;
+    this.isResetRate = true;
+    this.isResetBirthday = true;
   }
   checkRate(rate: number): string {
     if (this.isShowBadRate) {
@@ -65,6 +83,19 @@ export class AppComponent {
   deleteStudent(): void {
     this.filteredStudents.splice(this.studentId, 1);
     this.showPopup(-1);
+  }
+  private toggleElement( toggledElement: boolean): boolean {
+    if (toggledElement) {
+      return false;
+    }
+    return true;
+  }
+  private convertToDate(dateStr: string): Date {
+    const day = Number(dateStr.slice(0, 2)),
+          month = Number(dateStr.slice(3, 5)),
+          year = Number(dateStr.slice(6, 10));
+
+    return new Date(year, month, day);
   }
 }
 
